e789991ec42045eeaf5cdb23ef633aa6ef76a1cf
LarissaCarrara <larissa.carrara@hotmail.com>
2023-05-10 11:22:39 -0300

    projeto pizzaria com views

--- desenvolvimento_sistemas/2des/banco_dados/pizzaria2.0/procedimentos.sql
+++ desenvolvimento_sistemas/2des/banco_dados/pizzaria2.0/procedimentos.sql
@@ -10,3 +10,16 @@ INSERT INTO Itens_Pedido VALUES(27, 27, 1, 32.29);
 SELECT * from pizzas where nome = "Napolitana";  
 
 INSERT INTO Itens_Pedido VALUES(27, 28, 1, 33.41);
+
+-- CLiente será luiza de Melo quer 2 pizzas Napolitanas , passando 3 dados: client_id, pizza_id, qtd
+
+DROP PROCEDURE IF EXISTS sp_inserir_pedido;
+DELIMITER $
+
+CREATE PROCEDURE sp_inserir_pedido(
+    client_id INT, pizza_id INT, quantidade INT
+)
+BEGIN 
+    DECLARE valor_total DECIMAL(10,2);
+    DECLARE pedido_id INT;
+    DECLARE valor DECIMAL(10,2);
