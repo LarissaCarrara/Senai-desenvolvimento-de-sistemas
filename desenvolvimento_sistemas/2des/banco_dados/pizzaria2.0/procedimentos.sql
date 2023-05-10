e336814beccb46863facf658b1e6965c47da5048
LarissaCarrara <larissa.carrara@hotmail.com>
2023-05-10 10:41:23 -0300

    Senai

---
+++ desenvolvimento_sistemas/2des/banco_dados/pizzaria2.0/procedimentos.sql
@@ -0,0 +1,12 @@
+-- O cliente walter paulo costenaro quer 1 pizza de atum e 2 pizzas napolitanas
+INSERT INTO Pedidos Values ("", 98, '2023-05-10', '09:50', 99.11);
+
+SELECT * clientes where nome = "Walter Paulo Costenaro";
+
+("Walter Paulo Costenaro","Rua Anisio Alfredo da Silva Junior",253,null,"Zambom",null);
+
+INSERT INTO Itens_Pedido VALUES(27, 27, 1, 32.29);
+
+SELECT * from pizzas where nome = "Napolitana";  
+
+INSERT INTO Itens_Pedido VALUES(27, 28, 1, 33.41);
